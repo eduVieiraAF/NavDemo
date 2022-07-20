f5198adb929511067b57e3e714a2470a514cceed
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-20 09:26:54 -0300

    Nav:
    → UI for verifying details;
    → EnterDetailsFragment.kt code complete;
    → VerifyDetailsFragment.kt code complete but will be bettered.

--- app/src/main/java/com/example/navdemo/EnterDetailsFragment.kt
+++ app/src/main/java/com/example/navdemo/EnterDetailsFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 
 
@@ -22,10 +25,40 @@ class EnterDetailsFragment : Fragment() {
     ): View? {
 
         val rootView = inflater.inflate(R.layout.fragment_enter_details, container, false)
-
+        val etName = rootView.findViewById<EditText>(R.id.et_name)
+        val etMobileNumber = rootView.findViewById<EditText>(R.id.et_mobile)
         val btnVerifyDetails = rootView.findViewById<Button>(R.id.btn_verify_details)
+
         btnVerifyDetails.setOnClickListener {
-            findNavController().navigate(R.id.action_enterDetailsFragment_to_verifyDetailsFragment)
+
+            val name = etName.text.toString()
+            val mobile = etMobileNumber.text.toString()
+
+            when {
+                name.isEmpty() -> Toast.makeText(
+                    activity,
+                    "Enter name",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                mobile.isEmpty() -> Toast.makeText(
+                    activity,
+                    "Enter mobile number",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                else -> {
+                    val bundle = bundleOf(
+                        "name" to name,
+                        "mobile" to mobile.toLong()
+                    )
+
+                    findNavController().navigate(
+                        R.id.action_enterDetailsFragment_to_verifyDetailsFragment,
+                        bundle
+                    )
+                }
+            }
         }
 
         return rootView
--- app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
+++ app/src/main/java/com/example/navdemo/VerifyDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class VerifyDetailsFragment : Fragment() {
 
@@ -16,7 +17,18 @@ class VerifyDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_verify_details, container, false)
+
+        val name = arguments?.getString("name")
+        val mobileNumber = arguments?.getLong("mobile")
+
+        val rootView = inflater.inflate(R.layout.fragment_verify_details, container, false)
+
+        val tvName = rootView.findViewById<TextView>(R.id.tv_name)
+        val tvMobileNumber =rootView.findViewById<TextView>(R.id.tv_mobile_number)
+
+        tvName.text = name
+        tvMobileNumber.text = mobileNumber.toString()
+
+        return rootView
     }
 }
